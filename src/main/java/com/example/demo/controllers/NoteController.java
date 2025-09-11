cc4754d17d4045a6ba489ba6ae60756d563022e3
IvanyosLaszlo <ivanyoslaszlo25@gmail.com>
2025-09-11 18:44:58 +0200

    Az összes sql metodus át téve a respository osztályba

--- src/main/java/com/example/demo/controllers/NoteController.java
+++ src/main/java/com/example/demo/controllers/NoteController.java
@@ -34,54 +34,27 @@ public class NoteController {
     }
 
     @GetMapping("/note")
-    public List<String> getNotes(HttpSession session) {
-
+    public Object getNotes(HttpSession session) {
         String username = (String) session.getAttribute("user");
-        List<String> notes = new ArrayList<>();
 
-        String url = "jdbc:sqlite:user.datas.db";
-        String sql = "SELECT content FROM notes WHERE username = ?";
-
-        try (Connection connection = DriverManager.getConnection(url);
-             PreparedStatement ps = connection.prepareStatement(sql)) {
-            ps.setString(1, username);
-            ResultSet rs = ps.executeQuery();
-
-            while (rs.next()) {
-                notes.add(rs.getString("content"));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        if (username == null) {
+            return "Hozzáférés megtagadva!";
         }
-
-        return notes;
+        return userRespository.getNotes(username);
     }
 
     @DeleteMapping("/note")
     public String deleteNotes(@RequestBody List<String> contents, HttpSession session) {
+
         String username = (String) session.getAttribute("user");
-        String url = "jdbc:sqlite:user.datas.db";
-        String sql = "DELETE FROM notes WHERE username = ? AND content = ?";
 
-        int totalDeleted = 0;
-
-        try (Connection connection = DriverManager.getConnection(url)) {
-            for (String content : contents) {
-                try (PreparedStatement ps = connection.prepareStatement(sql)) {
-                    ps.setString(1, username);
-                    ps.setString(2, content);
-                    totalDeleted += ps.executeUpdate();
-                }
-            }
-            if (totalDeleted > 0) {
-                System.out.println("Sikeresen törölve: " + totalDeleted);
-            } else {
-                System.out.println("Nem történt törlés");
-            }
-            return null;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        if (userRespository.deleteNotes(contents, username)) {
+            return "Sikeres törlés";
+        } else {
+            return "Sikertelen törlés";
         }
+
+
     }
 
 }
--- src/main/java/com/example/demo/respository/repository.java
+++ src/main/java/com/example/demo/respository/repository.java
@@ -1,18 +1,22 @@
 package com.example.demo.respository;
 
 import com.example.demo.entities.Users;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class repository {
 
     private final String url = "jdbc:sqlite:user.datas.db";
     private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
 
     public boolean check_username(String username) {
         String sql = "SELECT username FROM users WHERE username=?";
@@ -103,5 +107,59 @@ public class repository {
             throw new RuntimeException(e);
         }
     }
+
+
+    public List<String> getNotes(String username) {
+
+        List<String> notes = new ArrayList<>();
+
+        String url = "jdbc:sqlite:user.datas.db";
+        String sql = "SELECT content FROM notes WHERE username = ?";
+
+        try (Connection connection = DriverManager.getConnection(url);
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, username);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                notes.add(rs.getString("content"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return notes;
+
+    }
+
+
+    public boolean deleteNotes(List<String> contents,String username) {
+        String url = "jdbc:sqlite:user.datas.db";
+        String sql = "DELETE FROM notes WHERE username = ? AND content = ?";
+
+        int totalDeleted = 0;
+
+
+        try (Connection connection = DriverManager.getConnection(url)) {
+            for (String content : contents) {
+                try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                    ps.setString(1, username);
+                    ps.setString(2, content);
+                    totalDeleted += ps.executeUpdate();
+                }
+            }
+            if (totalDeleted > 0) {
+                System.out.println("Sikeresen törölve: " + totalDeleted);
+                return true;
+            } else {
+                System.out.println("Nem történt törlés");
+            }
+            return false;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 
 }
